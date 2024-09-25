ceff300f676a70a19a7ebf82c331a2e63724abdb
jeremyjerome <167481787+jeremyjerome099@users.noreply.github.com>
2024-09-25 14:07:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+export default {
+  server: {
+    port: 5173, // or your desired port
+    host: '0.0.0.0', // to allow external connections
+  }
+};
+
